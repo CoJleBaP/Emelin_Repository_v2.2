a40a4eb64d4e8f4e0af134e42918091ca4ff0ca4
CoJleBaP <leshaemelin@gmail.com>
2023-12-28 02:48:45 +0300

    tretya vetka

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int EnterNumberA(int a)
 {
-   
+    
 }
 
 int EnterNumberB(int b)
@@ -17,7 +17,7 @@ int EnterNumberB(int b)
 
 void OST(int a, int b)
 {
-    
+    cout << a % b;
 }
 
 void DEL(int a, int b)
@@ -28,11 +28,11 @@ void DEL(int a, int b)
 
 int main()
 {
-    cout << "Press 1 to enter A ";
-    cout << "Press 2 to enter B ";
-    cout << "Press 3 to have A%B ";
-    cout << "Press 4 to have B/A ";
-    cout << "Press 0 for end ";
+    cout << "Press 1 to enter A " << endl;
+    cout << "Press 2 to enter B " << endl;
+    cout << "Press 3 to have A%B " << endl;
+    cout << "Press 4 to have B/A " << endl;
+    cout << "Press 0 for end " << endl;
     int N, a = 0, b = 0, c = 0;
     cin >> N;
 
