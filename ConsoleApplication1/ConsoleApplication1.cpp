10d942277e2d505c8a1a8302977d51712ce22285
CoJleBaP <151513830+CoJleBaP@users.noreply.github.com>
2023-12-28 03:55:45 +0300

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,34 +5,40 @@
 using namespace std;
 int EnterNumberA(int a)
 {
-   
+    cout << "Enter A - ";
+    cin >> a;
+    cout << "A = " << a << "\n";
+    return a;
 }
 
 int EnterNumberB(int b)
 {
-    
+    cout << "Enter B - ";
+    cin >> b;
+    cout << "B = " << b << "\n";
+    return b;
 }
 
 
 
 void OST(int a, int b)
 {
-    
+    cout << a % b << endl;
 }
 
 void DEL(int a, int b)
 {
-    
+    cout << b / a << endl;
 }
 
 
 int main()
 {
-    cout << "Press 1 to enter A ";
-    cout << "Press 2 to enter B ";
-    cout << "Press 3 to have A%B ";
-    cout << "Press 4 to have B/A ";
-    cout << "Press 0 for end ";
+    cout << "Press 1 to enter A " << endl;
+    cout << "Press 2 to enter B " << endl;
+    cout << "Press 3 to have A%B " << endl;
+    cout << "Press 4 to have B/A " << endl;
+    cout << "Press 0 for end " << endl;
     int N, a = 0, b = 0, c = 0;
     cin >> N;
 
@@ -51,10 +57,10 @@ int main()
             cin >> N;
             continue;
         case 3:
-            OST(a,b);
+            OST(a, b);
             continue;
         case 4:
-            DEL(a,b);
+            DEL(a, b);
             continue;
         default:
             cout << "Programm is finished";
